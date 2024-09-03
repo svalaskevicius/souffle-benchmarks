93c9f8fc747e9ea1470652f5f52453740345b925
Sarunas Valaskevicius <rakatan@gmail.com>
2024-09-03 21:25:11 +0100

    sort programs

--- benchmarks/small_benchmark.sh
+++ benchmarks/small_benchmark.sh
@@ -2,7 +2,7 @@
 root="/home/sarunas/code"
 benchdir="$root/benchmarks/benchmarks"
 # programs=(tc topological_ordering pointsto)
-programs=(cprog1 pointsto access3 po1 cprog5 po4 cprog2 tc cellular_automata po5 x9 po3 orbits sequences ranpo access2 po2 tak components nfsa2fsa cprog4 trans family topological_ordering access1 access-policy cprog3)
+programs=( access1 access2 access3 access-policy cellular_automata components cprog1 cprog2 cprog3 cprog4 cprog5 family nfsa2fsa orbits po1 po2 po3 po4 po5 pointsto ranpo sequences tak tc topological_ordering trans x9)
 instance=$1
   
 cd $benchdir
